e9c2f7305e4e0ee59771b6f61fb84f47d3695b62
BoxbbBoxbBox <136146679+BoxbbBoxbBox@users.noreply.github.com>
2024-08-11 00:59:14 -0500

    Adjust margins, box-sizing, and header

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const container = document.querySelector(".container");
-let userInput = 16;
+let userInput = 100;
 let userCalculated = userInput ** 2;
 
 function addDivs(userCalculated) {
@@ -15,7 +15,7 @@ addDivs(userCalculated);
 
 const allSquares = document.querySelectorAll(".square");
 for (let i = 0; i < userCalculated; i++) {
-    allSquares[i].style.flexBasis = 960 / userCalculated * userInput - 2 + "px";
-    allSquares[i].style.height = 960 / userCalculated * userInput - 2 + "px";
+    allSquares[i].style.flexBasis = 960 / userCalculated * userInput + "px";
+    allSquares[i].style.height = 960 / userCalculated * userInput + "px";
 }
 
